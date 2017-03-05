6d03f554b8c060ac90c443825f8cb5d30756b445
Sydney Smith <sydmac@gmail.com>
2017-03-05 16:06:22 -0500

    added node and mongoose

---
+++ server.js
@@ -0,0 +1,37 @@
+    // set up ========================
+    var express  = require('express');
+    var app      = express();
+    var mongoose = require('mongoose');
+    var morgan = require('morgan');
+    var bodyParser = require('body-parser');
+    var methodOverride = require('method-override');
+    var db = require('./db');
+
+    // configuration =================
+
+
+   var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/grab-bag-app'
+
+    mongoose.connect(mongoURI);
+
+
+    // connect to mongoDB database on modulus.io
+
+    function quit() {
+      mongoose.disconnect();
+      console.log('All Done!');
+    }
+
+    app.use(express.static(__dirname + '/app'));
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({'extended':'true'}));
+    app.use(bodyParser.json());
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+    app.use(methodOverride());
+
+    // listen (start app with node server.js) ======================================
+
+
+
+    app.listen(8080);
+    console.log("App listening on port 8080");
