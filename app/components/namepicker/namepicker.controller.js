015aa68b9568a0d4541d89c1ba9e6b1ff66e6b7d
Sydney Smith <sydney_s_smith@homedepot.com>
2017-03-06 14:38:48 -0500

    working loop function for family names

--- app/components/namepicker/namepicker.controller.js
+++ app/components/namepicker/namepicker.controller.js
@@ -1,42 +1,47 @@
 angular.module('grabBagApp')
        .controller('namePickerCtrl', namePickerCtrl);
 
-       namePickerCtrl.$inject = ['$http', '$state'];
+namePickerCtrl.$inject = ['$http', '$state'];
 
-      function namePickerCtrl($http, $state) {
+function namePickerCtrl($http, $state) {
 
-        var ctrl = this;
+    var ctrl = this;
 
-        ctrl.people = [];
+    ctrl.people = [];
 
-        ctrl.getPeople = function() {
-            $http.get('/api/people').then(function(response) {
-                ctrl.people = response.data;
-                console.log('ctrl.people:', ctrl.people);
-            });
-        };
+    ctrl.getPeople = function() {
+        $http.get('/api/people').then(function(response) {
+            ctrl.people = response.data;
+            console.log('ctrl.people:', ctrl.people);
+        });
+    };
 
-        ctrl.getPeople();
+    ctrl.getPeople();
 
-        ctrl.grabBagNames = [];
-        ctrl.shuffleNames = shuffleNames;
-        ctrl.loop = loop;
-        ctrl.loopPressed = false;
+    ctrl.grabBagNames = [];
+    ctrl.shuffleNames = shuffleNames;
+    ctrl.loop = loop;
+    ctrl.loopPressed = false;
 
-        function shuffleNames (people){
-          var namePicker = people.sort(function(){ return 0.5 - Math.random() });
-          return namePicker;
-        }
+    function shuffleNames (people){
+      var namePicker = people.sort(function(){ return 0.5 - Math.random() });
+      return namePicker;
+    }
 
-         function loop(){
-          ctrl.namePicker = angular.copy(ctrl.people);
-          for(var i = 0; i < ctrl.people.length; i++){
-          for(var j = 0; j < ctrl.namePicker.length; j++)
-            while(ctrl.people[i].name === ctrl.namePicker[j].name){
-              ctrl.grabBagNames = ctrl.shuffleNames(ctrl.namePicker);
-              ctrl.loopPressed = true;
-              return ctrl.grabBagNames;
+    function loop(){
+      ctrl.namePicker = angular.copy(ctrl.people);
+      for(var i = 0; i < ctrl.people.length; i++){
+        for(var j = 0; j < ctrl.namePicker.length; j++){
+          while(ctrl.people[i].name === ctrl.namePicker[j].name){
+            for(var i = 0; i < ctrl.people[i].family.length; i++){
+              for (var j = 0; j < ctrl.namePicker[j].family.length; j++){
+                while(ctrl.people[i].family[i] === ctrl.namePicker[j].family[j])
+                    ctrl.grabBagNames = ctrl.shuffleNames(ctrl.namePicker);
+                    ctrl.loopPressed = true;
+                    return ctrl.grabBagNames;
+              }
             }
-          }
+          } 
         }
-};
+      }
+    };
--- models/person.js
+++ models/person.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var PersonSchema = new mongoose.Schema({
   name: String,
-  pic: String
+  pic: String,
+  family: Array
 });
 
 module.exports = mongoose.model('Person', PersonSchema);
