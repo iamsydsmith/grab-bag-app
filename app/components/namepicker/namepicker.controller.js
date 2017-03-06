76f6f2bc51443c3bc854f50c1a53022c73eb8a89
Sydney Smith <sydmac@gmail.com>
2017-03-05 21:55:47 -0500

    fixing name picker

--- app/components/namepicker/namepicker.controller.js
+++ app/components/namepicker/namepicker.controller.js
@@ -18,50 +18,6 @@ angular.module('grabBagApp')
 
         ctrl.getPeople();
 
-        // ctrl.people = [
-        //  { name: "Sydney",
-        //    pic: "pics/syd.jpg"
-        //   },
-        //   {
-        //     name: "Tamara",
-        //     pic: "pics/tam.jpg"
-        //   },
-        //   {
-        //     name: "Talisha",
-        //     pic: "pics/talisha.jpg"
-        //   },
-        //   {
-        //     name: "Bertha",
-        //     pic: "pics/bert.jpg"
-        //   },
-        //   {
-        //     name: "Devin",
-        //     pic: "pics/devin.jpg"
-        //   },
-        //   {
-        //     name: "Michelle",
-        //     pic: "pics/michelle.jpg"
-        //   },
-        //   {
-        //     name: "Linda",
-        //     pic: "pics/linda.jpg"
-        //   },
-        //   {
-        //     name: "Larry",
-        //     pic: "pics/larry.jpg"
-        //   },
-        //   {
-        //     name: "Terry",
-        //     pic: "pics/terry.jpg"
-
-        //   },
-        //   {
-        //     name: "Sean",
-        //     pic: "pics/sean.jpg"
-        //   }
-        // ];
-
-        ctrl.namePicker = angular.copy(ctrl.people);
         ctrl.grabBagNames = [];
         ctrl.shuffleNames = shuffleNames;
         ctrl.loop = loop;
@@ -73,6 +29,7 @@ angular.module('grabBagApp')
         }
 
          function loop(){
+          ctrl.namePicker = angular.copy(ctrl.people);
           for(var i = 0; i < ctrl.people.length; i++){
           for(var j = 0; j < ctrl.namePicker.length; j++)
             while(ctrl.people[i].name === ctrl.namePicker[j].name){
