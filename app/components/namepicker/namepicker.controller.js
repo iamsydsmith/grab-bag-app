ef1fb5af0b0e60e1bd744a25df59feb289df346c
Sydney Smith <sydney_s_smith@homedepot.com>
2017-03-12 20:32:44 -0400

    updated loop function

--- app/components/namepicker/namepicker.controller.js
+++ app/components/namepicker/namepicker.controller.js
@@ -30,16 +30,16 @@ function namePickerCtrl($http, $state) {
 
     function loop(){
       ctrl.namePicker = angular.copy(ctrl.people);
-      while(ctrl.people[0].name == ctrl.people[0].name &&
-        ctrl.people[1].name == ctrl.people[1].name &&
-        ctrl.people[2].name == ctrl.people[2].name &&
-        ctrl.people[3].name == ctrl.people[3].name &&
-        ctrl.people[4].name == ctrl.people[4].name &&
-        ctrl.people[5].name == ctrl.people[5].name &&
-        ctrl.people[6].name == ctrl.people[6].name &&
-        ctrl.people[7].name == ctrl.people[7].name &&
-        ctrl.people[8].name == ctrl.people[8].name &&
-        ctrl.people[9].name == ctrl.people[9].name &&){
+      while(ctrl.people[0].name == ctrl.namePicker[0].name &&
+        ctrl.people[1].name == ctrl.namePicker[1].name &&
+        ctrl.people[2].name == ctrl.namePicker[2].name &&
+        ctrl.people[3].name == ctrl.namePicker[3].name &&
+        ctrl.people[4].name == ctrl.namePicker[4].name &&
+        ctrl.people[5].name == ctrl.namePicker[5].name &&
+        ctrl.people[6].name == ctrl.namePicker[6].name &&
+        ctrl.people[7].name == ctrl.namePicker[7].name &&
+        ctrl.people[8].name == ctrl.namePicker[8].name &&
+        ctrl.people[9].name == ctrl.namePicker[9].name){
         ctrl.grabBagNames = ctrl.shuffleNames(ctrl.namePicker);
         ctrl.loopPressed = true;
         return ctrl.grabBagNames;
