b4294d1fb9beac49881c5d68ab8423f74a9d9063
Sydney Smith <sydney_s_smith@homedepot.com>
2017-03-12 20:38:31 -0400

    updated loop function

--- app/components/namepicker/namepicker.controller.js
+++ app/components/namepicker/namepicker.controller.js
@@ -29,20 +29,21 @@ function namePickerCtrl($http, $state) {
     }
 
     function loop(){
-      ctrl.namePicker = angular.copy(ctrl.people);
-      while(ctrl.people[0].name == ctrl.namePicker[0].name &&
-        ctrl.people[1].name == ctrl.namePicker[1].name &&
-        ctrl.people[2].name == ctrl.namePicker[2].name &&
-        ctrl.people[3].name == ctrl.namePicker[3].name &&
-        ctrl.people[4].name == ctrl.namePicker[4].name &&
-        ctrl.people[5].name == ctrl.namePicker[5].name &&
-        ctrl.people[6].name == ctrl.namePicker[6].name &&
-        ctrl.people[7].name == ctrl.namePicker[7].name &&
-        ctrl.people[8].name == ctrl.namePicker[8].name &&
-        ctrl.people[9].name == ctrl.namePicker[9].name){
-        ctrl.grabBagNames = ctrl.shuffleNames(ctrl.namePicker);
-        ctrl.loopPressed = true;
-        return ctrl.grabBagNames;
+        ctrl.namePicker = angular.copy(ctrl.people);
+        ctrl.grabBagNames = ctrl.shuffleNames(ctrl.namePicker)
+        while(ctrl.people[0].name == ctrl.grabBagNames[0].name &&
+            ctrl.people[1].name == ctrl.grabBagNames[1].name &&
+            ctrl.people[2].name == ctrl.grabBagNames[2].name &&
+            ctrl.people[3].name == ctrl.grabBagNames[3].name &&
+            ctrl.people[4].name == ctrl.grabBagNames[4].name &&
+            ctrl.people[5].name == ctrl.grabBagNames[5].name &&
+            ctrl.people[6].name == ctrl.grabBagNames[6].name &&
+            ctrl.people[7].name == ctrl.grabBagNames[7].name &&
+            ctrl.people[8].name == ctrl.grabBagNames[8].name &&
+            ctrl.people[9].name == ctrl.grabBagNames[9].name){
+            ctrl.grabBagNames = ctrl.shuffleNames(ctrl.grabBagNames);
+            ctrl.loopPressed = true;
+            return ctrl.grabBagNames;
       }
     }
 };
