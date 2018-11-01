cd60be86d9eb1985686102a64f5e51be2d53f512
sydneysmith <sydney.smith@turner.com>
2018-10-31 21:29:22 -0400

    fixing heroku deploy for back-end

--- back-end/routes/people.js
+++ back-end/routes/people.js
@@ -39,10 +39,10 @@ router.post('/', function (req, res, next) {
 
 router.patch('/:id', function (req, res, next) {
   Person.findById(req.params.id, (err, person) => {
-    if(req.body._id) {
+    if (req.body._id) {
       delete req.body._id;
     }
-    for(let b in req.body){
+    for (let b in req.body) {
       person[b] = req.body[b];
     }
     person.save();
@@ -55,6 +55,16 @@ router.delete('/:id', function (req, res, next) {
   Person.findOneAndRemove({ _id: req.params.id })
   .then(() => {
     res.send("family member was deleted")
+  })
+  .catch(err => {
+    res.status(400).send("unable to save to database");
+  });
+});
+
+router.delete('/deleteAll', function (req, res, next) {
+  Person.drop({})
+  .then(() => {
+    res.send("All family members were deleted")
   })
   .catch(err => {
     res.status(400).send("unable to save to database");
