a7a83685733db145fa53bbd3b47040251788ef37
sydneysmith <sydney.smith@turner.com>
2018-11-01 10:40:02 -0400

    fixing app set up

--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
-import App from './components/App.js';
-
-const familyUrl = 'http://mac-grab-bag.herokuapp.com/family'
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
