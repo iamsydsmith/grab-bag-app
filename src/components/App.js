6a2e7d10830393bbc3b832deaca9148a845de8a8
sydneysmith <sydney.smith@turner.com>
2018-11-01 10:42:02 -0400

    set up complete

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Learn React
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
